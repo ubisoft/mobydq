9a8ca8a3691b8d4f33cbd002939c7827efa18869
alexisrolland <alexisrolland@hotmail.com>
2017-11-11 13:31:17 +0800

    Updated unit test case name formatting for better readability.

--- test/test_utils.py
+++ test/test_utils.py
@@ -14,9 +14,9 @@ sys.path.insert(0, parent_directory)
 
 def test_case_name(test_case_list):
     """Generate unique name for unit test case."""
-    test_case_name = 'Unit Test {}'.format(datetime.now().strftime("%Y%m%d%H%M%S"))
+    test_case_name = 'unit_test_{}'.format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
     for test_case in test_case_list:
         if test_case_name in test_case['test_case']:
             time.sleep(1)
-            test_case_name = 'Unit Test {}'.format(datetime.now().strftime("%Y%m%d%H%M%S"))
+            test_case_name = 'unit_test_{}'.format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
     return test_case_name
