9c9856975dcc7d6330def88c64f151c3aa5a4739
alexisrolland <alexisrolland@hotmail.com>
2018-01-20 21:28:36 +0800

    Added logging to graphql schema files

--- api/event_schema.py
+++ api/event_schema.py
@@ -1,6 +1,10 @@
 from graphene_sqlalchemy import SQLAlchemyObjectType
 from database.event import EventType as EventTypeModel, Event as EventModel
 import graphene
+import logging
+
+# Load logging configuration
+log = logging.getLogger(__name__)
 
 
 class EventType(SQLAlchemyObjectType):
--- api/run_graphql_api.py
+++ api/run_graphql_api.py
@@ -1,12 +1,22 @@
 from database.base import dbSession
 from flask import Flask
+from flask_cors import CORS
 from flask_graphql import GraphQLView
 from schema import schema
+import api_utils
+import logging
+import sys
+
+log = logging.getLogger(__name__)
+logging.basicConfig(
+    # filename='data_quality.log',
+    stream=sys.stdout,
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 app = Flask(__name__)
-app.debug = True
-
-app.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True))
+CORS(app)
+app.add_url_rule('/dataquality/api/graphql', view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True))
 
 
 @app.teardown_appcontext
@@ -15,4 +25,6 @@ def shutdown_session(exception=None):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    api_utils.init()
+    config = api_utils.get_parameter('api')
+    app.run(host=config['host'], port=int(config['port']), threaded=True, debug=True)
