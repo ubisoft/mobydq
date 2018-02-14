be8ce2bfa916ab3bbd0bdb339d6e5ae5be91c6fd
Alexis Rolland <alexis.rolland@ubisoft.com>
2018-02-14 14:07:40 +0800

    Removed expire_on_commit=false in base.py as an attempt to stabilize the build.

--- api/database/base.py
+++ api/database/base.py
@@ -19,7 +19,7 @@ Base = declarative_base()
 Base.metadata.bind = engine  # Bind engine to metadata of the base class
 
 # Create database session object
-db_session = scoped_session(sessionmaker(bind=engine, expire_on_commit=False))
+db_session = scoped_session(sessionmaker(bind=engine))
 Base.query = db_session.query_property()  # Used by graphql to execute queries
 
 
