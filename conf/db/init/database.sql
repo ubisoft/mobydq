f7401810addb78087a9f825646a6f920580db990
alexisrolland <alexisrolland@hotmail.com>
2018-07-28 22:37:12 +0800

    Added function to update updated_date columns. Added triggers on tables to call function update_updated_date(). Added default values for data source types, indicator types, parameter types.

--- conf/db/init/database.sql
+++ conf/db/init/database.sql
@@ -1,17 +1,45 @@
+/*Create database*/
 CREATE DATABASE data_quality;
 \connect data_quality;
+
+
+/*Create schema*/
 CREATE SCHEMA base;
 
 
-/*Create tables*/
+/*Create functions*/
+CREATE OR REPLACE FUNCTION base.update_updated_date_column()
+RETURNS TRIGGER AS $$
+BEGIN
+   NEW.updated_date = now();
+   RETURN NEW;
+END;
+$$ language 'plpgsql';
+
+
+/*Create table data source type*/
 CREATE TABLE base.data_source_type (
     id SERIAL PRIMARY KEY,
     data_source_type TEXT NOT NULL,
     created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
 COMMENT ON TABLE base.data_source_type IS
 'Data source types describe the types of a data sources the data quality framework can connect to.';
+
+CREATE TRIGGER data_source_type_updated_date BEFORE UPDATE
+ON base.data_source_type FOR EACH ROW EXECUTE PROCEDURE
+base.update_updated_date_column();
+
+INSERT INTO base.data_source_type (data_source_type) VALUES
+('Hive'),
+('Impala'),
+('Microsoft SQL Server'),
+('MySQL'),
+('PostgreSQL'),
+('SQLite'),
+('Teradata');
 
 
 CREATE TABLE base.data_source (
@@ -24,8 +52,13 @@ CREATE TABLE base.data_source (
     updated_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     data_source_type_id INTEGER NOT NULL REFERENCES base.data_source_type(id)
 );
+
 COMMENT ON TABLE base.data_source IS
 'Data sources are systems containing or exposing data on which the data quality framework can compute indicators.';
+
+CREATE TRIGGER data_source_updated_date BEFORE UPDATE
+ON base.data_source FOR EACH ROW EXECUTE PROCEDURE
+base.update_updated_date_column();
 
 
 CREATE TABLE base.indicator_type (
@@ -35,8 +68,19 @@ CREATE TABLE base.indicator_type (
     created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
 COMMENT ON TABLE base.indicator_type IS
 'Indicator types determine which function of the data quality framework is used to compute indicators.';
+
+CREATE TRIGGER indicator_type_updated_date BEFORE UPDATE
+ON base.indicator_type FOR EACH ROW EXECUTE PROCEDURE
+base.update_updated_date_column();
+
+INSERT INTO base.indicator_type (indicator_type, function) VALUES
+('Completeness', 'evaluate_completeness'),
+('Freshness', 'evaluate_freshness'),
+('Latency', 'evaluate_latency'),
+('Validity', 'evaluate_validity');
 
 
 CREATE TABLE base.indicator_group (
@@ -45,8 +89,13 @@ CREATE TABLE base.indicator_group (
     created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
 COMMENT ON TABLE base.indicator_group IS
 'Indicator groups define collections of indicators to be computed in the same batch.';
+
+CREATE TRIGGER indicator_group_updated_date BEFORE UPDATE
+ON base.indicator_group FOR EACH ROW EXECUTE PROCEDURE
+base.update_updated_date_column();
 
 
 CREATE TABLE base.indicator (
@@ -60,8 +109,13 @@ CREATE TABLE base.indicator (
     indicator_type_id INTEGER NOT NULL REFERENCES base.indicator_type(id),
     indicator_group_id INTEGER NOT NULL REFERENCES base.indicator_group(id)
 );
+
 COMMENT ON TABLE base.indicator IS
 'Indicators compute data sets on one or several data sources in order to evaluate the quality of their data.';
+
+CREATE TRIGGER indicator_updated_date BEFORE UPDATE
+ON base.indicator FOR EACH ROW EXECUTE PROCEDURE
+base.update_updated_date_column();
 
 
 CREATE TABLE base.parameter_type (
@@ -71,8 +125,24 @@ CREATE TABLE base.parameter_type (
     created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
 COMMENT ON TABLE base.parameter_type IS
 'Parameter types determine which types of parameters can be used to compute indicators.';
+
+CREATE TRIGGER parameter_type_updated_date BEFORE UPDATE
+ON base.parameter_type FOR EACH ROW EXECUTE PROCEDURE
+base.update_updated_date_column();
+
+INSERT INTO base.parameter_type (parameter_type, description) VALUES
+('Alert operator', 'Operator used to compare the results of the indicator with the alert threshold. Example: =, >, >=, <, <=, <>'),
+('Alert threshold', 'Numeric value used to evaluate the results of the indicator and determine if an alert must be sent.'),
+('Distribution list', 'List of e-mail addresses to which alerts must be sent. Example: [''email_1'', ''email_2'', ''email_3'']'),
+('Dimension', 'List of values to indicate dimensions in the results of the indicator. Example: [''dimension_1'', ''dimension_2'', ''dimension_3'']'),
+('Measure', 'List of values to indicate measures in the results of the indicator. Example: [''measure_1'', ''measure_2'', ''measure_3'']'),
+('Source', 'Name of the data source which serves as a reference to evaluate the quality of the data.'),
+('Source request', 'SQL query used to compute the indicator on the source system.'),
+('Target', 'Name of the data source on which to evaluate the quality of the data.'),
+('Target request', 'SQL query used to compute the indicator on the target system.');
 
 
 CREATE TABLE base.parameter (
@@ -83,8 +153,13 @@ CREATE TABLE base.parameter (
     parameter_type_id INTEGER NOT NULL REFERENCES base.parameter_type(id),
     indicator_id INTEGER NOT NULL REFERENCES base.indicator(id)
 );
+
 COMMENT ON TABLE base.parameter IS
 'Parameters used by the data quality framework to compute indicators.';
+
+CREATE TRIGGER parameter_updated_date BEFORE UPDATE
+ON base.parameter FOR EACH ROW EXECUTE PROCEDURE
+base.update_updated_date_column();
 
 
 CREATE TABLE base.batch (
@@ -92,6 +167,7 @@ CREATE TABLE base.batch (
     created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     indicator_group_id INTEGER NOT NULL REFERENCES base.indicator_group(id)
 );
+
 COMMENT ON TABLE base.batch IS
 'Batches record the execution of groups of indicators.';
 
@@ -104,8 +180,13 @@ CREATE TABLE base.session (
     batch_id INTEGER NOT NULL REFERENCES base.batch(id),
     indicator_id INTEGER NOT NULL REFERENCES base.indicator(id)
 );
+
 COMMENT ON TABLE base.session IS
 'Sessions record the execution of indicators within a batch.';
+
+CREATE TRIGGER session_updated_date BEFORE UPDATE
+ON base.session FOR EACH ROW EXECUTE PROCEDURE
+base.update_updated_date_column();
 
 
 CREATE TABLE base.indicator_result (
@@ -119,5 +200,6 @@ CREATE TABLE base.indicator_result (
     session_id INTEGER NOT NULL REFERENCES base.session(id),
     indicator_id INTEGER NOT NULL REFERENCES base.indicator(id)
 );
+
 COMMENT ON TABLE base.indicator_result IS
 'Indicator results contain a summary of indicators execution.';
