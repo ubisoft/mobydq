99978a925d6a741ed1860061412735862f54aedf
alexisrolland <alexisrolland@hotmail.com>
2018-08-13 00:54:40 +0800

    Update port in module scripts, method utils.execute_graphql_request()

--- scripts/utils.py
+++ scripts/utils.py
@@ -3,7 +3,7 @@ import requests
 
 def execute_graphql_request(payload):
     """Execute queries and mutations on the GraphQL API."""
-    url = 'http://graphql:5000/graphql'  # Should be moved to config file
+    url = 'http://graphql:5433/graphql'  # Should be moved to config file
     headers = {'Content-Type': 'application/graphql'}
     response = requests.post(url, headers=headers, data=payload)
     data = response.json()
