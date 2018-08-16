92d4faca0862f26638142a334ce5e74302f7b071
Vilkazz <baka367@gmail.com>
2018-08-17 01:44:28 +0800

    Replace empty app with sample materialUi dashboard app

--- app/src/index.js
+++ app/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Dashboard from './Dashboard';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Dashboard />, document.getElementById('root'));
 registerServiceWorker();
