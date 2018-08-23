7c13b7dcda63c3354daf81babdb7a89590de7d92
Vilkazz <baka367@gmail.com>
2018-08-24 01:43:57 +0800

    Implemented sample table to display indicator list

--- app/src/Components/Base/Root.js
+++ app/src/Components/Base/Root.js
@@ -2,14 +2,24 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 import BaseDataView from './BaseDataView'
 
+
+const client = new ApolloClient({
+  uri: "http://192.168.99.100:5433/graphql",
+opts: { mode: 'no-cors' }
+});
+
 const Root = ({ store }) => (
-  <Provider store={store}>
-    <BrowserRouter>
-      <BaseDataView/>
-    </BrowserRouter>
-  </Provider>
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <BaseDataView/>
+      </BrowserRouter>
+    </Provider>
+  </ApolloProvider>
 )
 
 Root.propTypes = {
