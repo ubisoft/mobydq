d9df4c5e313b4016f0087a5a6a25ed5205a26694
alexisrolland <alexisrolland@hotmail.com>
2018-09-05 23:51:54 +0800

    Added unit tests for data source methods

--- test/test_scripts/test_data_source.py
+++ test/test_scripts/test_data_source.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 from scripts.init.data_source import DataSource
-from scripts.init import utils
 import time
 import unittest
 
@@ -17,17 +16,79 @@ class TestDataSource(unittest.TestCase):
         test_case_name = 'test {}'.format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
         return test_case_name
 
+    def test_get_connection_sql_server(self):
+        # Set connection parameters
+        data_source_type_id = 4
+        connection_string = 'driver={FreeTDS};server=0.0.0.0;port=9000;database=star_wars;tds_version=8.0;'
+        login = 'sa'
+        password = '1234-abcd'
+
+        # Connect to test database
+        data_source = DataSource()
+        connection = data_source.get_connection(data_source_type_id, connection_string, login, password)
+        cursor = connection.cursor()
+        result = cursor.execute("SELECT name FROM star_wars.dbo.planet WHERE name='Tatooine';").fetchone()
+        result = result[0]
+        cursor.close()
+        connection.close()
+
+        # Assert query result
+        self.assertEqual(result, 'Tatooine')
+
     def test_get_connection_mysql(self):
-        pass
+        # Set connection parameters
+        data_source_type_id = 5
+        connection_string = 'driver={MySQL Unicode};server=0.0.0.0;port=9001;database=star_wars;'
+        login = 'root'
+        password = '1234'
+
+        # Connect to test database
+        data_source = DataSource()
+        connection = data_source.get_connection(data_source_type_id, connection_string, login, password)
+        cursor = connection.cursor()
+        result = cursor.execute("SELECT name FROM star_wars.planet WHERE name='Tatooine';").fetchone()
+        result = result[0]
+        cursor.close()
+        connection.close()
+
+        # Assert query result
+        self.assertEqual(result, 'Tatooine')
 
     def test_get_connection_postgresql(self):
-        pass
+        # Set connection parameters
+        data_source_type_id = 7
+        connection_string = 'driver={PostgreSQL Unicode};server=0.0.0.0;port=9002;database=star_wars;'
+        login = 'postgres'
+        password = '1234'
 
-    def test_get_connection_sql_server(self):
-        pass
+        # Connect to test database
+        data_source = DataSource()
+        connection = data_source.get_connection(data_source_type_id, connection_string, login, password)
+        cursor = connection.cursor()
+        result = cursor.execute("SELECT name FROM public.planet WHERE name='Tatooine';").fetchone()
+        result = result[0]
+        cursor.close()
+        connection.close()
+
+        # Assert query result
+        self.assertEqual(result, 'Tatooine')
 
     def test_get_connection_sqlite(self):
-        pass
+        # Set connection parameters
+        data_source_type_id = 8
+        connection_string = './test/db-sqlite/star_wars.db'
+
+        # Connect to test database
+        data_source = DataSource()
+        connection = data_source.get_connection(data_source_type_id, connection_string)
+        cursor = connection.cursor()
+        result = cursor.execute("SELECT name FROM planet WHERE name='Tatooine';").fetchone()
+        result = result[0]
+        cursor.close()
+        connection.close()
+
+        # Assert query result
+        self.assertEqual(result, 'Tatooine')
 
     def test_get_connection_teradata(self):
         pass
