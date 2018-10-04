3e8a6abbee8b02a8f909314bfeb7503ae3761d0c
Thomas Gassmann <thomas.gassmann@hotmail.com>
2018-10-04 09:29:03 +0800

    rename to mobydq

--- api/init/data_source.py
+++ api/init/data_source.py
@@ -14,21 +14,23 @@ class DataSource:
         pass
 
     def test(self, data_source_id):
-        container_name = 'data-quality-test-data-source-{data_source_id}'.format(data_source_id=data_source_id)
+        container_name = 'mobydq-test-data-source-{data_source_id}'.format(
+            data_source_id=data_source_id)
         client = docker.from_env()
         client.containers.run(
             name=container_name,
-            image='data-quality-scripts',
-            network='data-quality-network',
-            links={'data-quality-graphql': 'data-quality-graphql'},
+            image='mobydq-scripts',
+            network='mobydq-network',
+            links={'mobydq-graphql': 'mobydq-graphql'},
             command=['python', 'run.py', 'test_data_source', data_source_id],
             stream=True,
             remove=True
-            )
+        )
 
         # Get connectivity test result
         query = '''query{dataSourceById(id:data_source_id){id,connectivityStatus}}'''
-        query = query.replace('data_source_id', str(data_source_id))  # Use replace() instead of format() because of curly braces
+        query = query.replace('data_source_id', str(
+            data_source_id))  # Use replace() instead of format() because of curly braces
         data = utils.execute_graphql_request(query)
 
         return data
