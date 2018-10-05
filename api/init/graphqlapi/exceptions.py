b20cba87fd6b05d8973ed6d79781b5e867d8bd5d
alexisrolland <alexisrolland@hotmail.com>
2018-10-05 21:35:58 +0800

    Added docstring and formatting

--- api/init/graphqlapi/exceptions.py
+++ api/init/graphqlapi/exceptions.py
@@ -2,12 +2,13 @@ from flask import jsonify, make_response
 
 
 class RequestException(Exception):
+    """Method to manage errors when sending http request to GraphQL."""
 
-    def __init__(self, code: int, message: object):
-        self._code = code
-        if type(message) == str:
-            message = {'message': message}
-        self._message = message
+    def __init__(self, status: int, response: object):
+        self._status = status
+        if type(response) == str:
+            response = {'message': response}
+        self._response = response
 
     def to_response(self):
-        return make_response(jsonify(self._message), self._code)
+        return make_response(jsonify(self._response), self._status)
