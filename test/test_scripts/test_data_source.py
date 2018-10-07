783c740e7307788afb6c6f8da2f9d2b54f275151
Pascal Honegger <pascal.honegger@bluewin.ch>
2018-10-07 23:53:33 +0800

    #122: Execute CircleCI commands even if previous command failed

--- test/test_scripts/test_data_source.py
+++ test/test_scripts/test_data_source.py
@@ -20,11 +20,11 @@ class TestDataSource(unittest.TestCase):
     def test_get_connection_sql_server(self):
         # Set connection parameters
         data_source_type_id = DataSourceType.MSSQL_ID
-        connection_string = 'driver={FreeTDS};server=db-sql-server;port=1433;DataSourceType=star_wars;tds_version=8.0;'
+        connection_string = 'driver={FreeTDS};server=db-sql-server;port=1433;Database=star_wars;tds_version=8.0;'
         login = 'sa'
         password = '1234-abcd'
 
-        # Connect to test DataSourceType
+        # Connect to test Database
         data_source = DataSource()
         connection = data_source.get_connection(data_source_type_id, connection_string, login, password)
         cursor = connection.cursor()
@@ -39,11 +39,11 @@ class TestDataSource(unittest.TestCase):
     def test_get_connection_mysql(self):
         # Set connection parameters
         data_source_type_id = DataSourceType.MYSQL_ID
-        connection_string = 'driver={MySQL Unicode};server=db-mysql;port=3306;DataSourceType=star_wars;'
+        connection_string = 'driver={MySQL Unicode};server=db-mysql;port=3306;Database=star_wars;'
         login = 'root'
         password = '1234'
 
-        # Connect to test DataSourceType
+        # Connect to test Database
         data_source = DataSource()
         connection = data_source.get_connection(data_source_type_id, connection_string, login, password)
         cursor = connection.cursor()
@@ -58,11 +58,11 @@ class TestDataSource(unittest.TestCase):
     def test_get_connection_mariadb(self):
         # Set connection parameters
         data_source_type_id = DataSourceType.MARIADB_ID
-        connection_string = 'driver={MariaDB Unicode};server=db-mariadb;port=3306;DataSourceType=star_wars;'
+        connection_string = 'driver={MariaDB Unicode};server=db-mariadb;port=3306;Database=star_wars;'
         login = 'root'
         password = '1234'
 
-        # Connect to test DataSourceType
+        # Connect to test Database
         data_source = DataSource()
         connection = data_source.get_connection(data_source_type_id, connection_string, login, password)
         cursor = connection.cursor()
@@ -77,11 +77,11 @@ class TestDataSource(unittest.TestCase):
     def test_get_connection_postgresql(self):
         # Set connection parameters
         data_source_type_id = DataSourceType.POSTGRESQL_ID
-        connection_string = 'driver={PostgreSQL Unicode};server=db-postgresql;port=5432;DataSourceType=star_wars;'
+        connection_string = 'driver={PostgreSQL Unicode};server=db-postgresql;port=5432;Database=star_wars;'
         login = 'postgres'
         password = '1234'
 
-        # Connect to test DataSourceType
+        # Connect to test Database
         data_source = DataSource()
         connection = data_source.get_connection(data_source_type_id, connection_string, login, password)
         cursor = connection.cursor()
@@ -98,7 +98,7 @@ class TestDataSource(unittest.TestCase):
         data_source_type_id = DataSourceType.SQLITE_ID
         connection_string = './star_wars.db'
 
-        # Connect to test DataSourceType
+        # Connect to test Database
         data_source = DataSource()
         connection = data_source.get_connection(data_source_type_id, connection_string)
         cursor = connection.cursor()
