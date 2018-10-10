eb54c51575adc50b86326a68a501599b33bd938f
simasjoneliunas <baka367@gmail.com>
2018-10-10 19:46:46 +0800

    add form input test

---
+++ app/src/tests/FormInput/TextInput.test.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import renderer from 'react-test-renderer';
+
+import TextInput from '../../Components/FormInput/TextInput';
+
+
+it('renders TextInput without crashing with good props', () => {
+  const data = "not an array";
+
+
+  const component = renderer.create(
+    <TextInput
+          id="name"
+          label="Indicator name"
+          helperText=""
+          placeholder="Enter indicator name"
+          touched={null}
+          error={true}
+          value="value"
+          onChange={null}
+          onBlur={null}
+          style={{ float: 'left' }}
+        />
+  );
+
+  let tree = component.toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
+
