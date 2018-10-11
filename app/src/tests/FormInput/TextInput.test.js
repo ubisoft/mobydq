ff18f13e2e6ed02cf3fd93eadf5f9378536b2773
simasjoneliunas <baka367@gmail.com>
2018-10-11 10:04:44 +0800

    Finished jest part of FormInputComponent test

--- app/src/tests/FormInput/TextInput.test.js
+++ app/src/tests/FormInput/TextInput.test.js
@@ -28,4 +28,76 @@ it('renders TextInput without crashing with good props', () => {
   expect(tree).toMatchSnapshot();
 });
 
+it('renders TextInput without crashing with good props and error set', () => {
+  const data = "not an array";
+
+
+  const component = renderer.create(
+    <TextInput
+          id="Id"
+          label="Label"
+          helperText="Helper Text"
+          placeholder="Enter indicator name"
+          touched={null}
+          error="an error text"
+          value="value"
+          onChange={null}
+          onBlur={null}
+          style={{ float: 'left' }}
+        />
+  );
+
+  let tree = component.toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
+
+it('renders TextInput without crashing if id is not set or value is empty', () => {
+  const data = "not an array";
+
+
+  const component = renderer.create(
+    <TextInput
+          id=""
+          label="Label"
+          helperText="Helper Text"
+          placeholder="Enter indicator name"
+          touched={null}
+          error="an error text"
+          value=""
+          onChange={null}
+          onBlur={null}
+          style={{ float: 'left' }}
+        />
+  );
+
+  let tree = component.toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders a numeric TextInput without crashing', () => {
+  const data = "not an array";
+
+
+  const component = renderer.create(
+    <TextInput
+          id="Id"
+          label="Label"
+          helperText="Helper Text"
+          placeholder="Enter indicator name"
+          touched={null}
+          error="an error text"
+          value="1323"
+          onChange={null}
+          onBlur={null}
+          numeric
+        />
+  );
+
+  let tree = component.toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
 
