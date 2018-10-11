877a13379e497883a05ff2ca87e3ad191b92a234
Vilkazz <baka367@gmail.com>
2018-10-12 00:24:06 +0800

    #119: Fixed CI errors

--- app/src/tests/Dashboard.test.js
+++ app/src/tests/Dashboard.test.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import * as Adapter from 'enzyme-adapter-react-16';
 import { shallow } from 'enzyme';
-import { shallowToJson } from 'enzyme-to-json';
 
 import Dashboard from './../Components/Dashboard/Dashboard';
 
