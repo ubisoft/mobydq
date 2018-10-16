3d91354bbfb2764e7a478dfa2bec49cda1c2a940
Eric Zuercher <ericz993@gmail.com>
2018-10-16 15:22:28 +0800

    Client permission check.

--- app/src/Components/Base/Content.js
+++ app/src/Components/Base/Content.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Dashboard from './../Dashboard/Dashboard';
 import Indicator from './../Indicator/Indicator';
 import IndicatorGroup from './../IndicatorGroup/IndicatorGroup';
@@ -8,14 +8,51 @@ import DataSource from './../DataSource/DataSource';
 import NotFoundComponent from '../Error/NotFoundComponent'
 import Login from '../Login/Login';
 
+const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route {...rest} render={(props) => (
+        isAuthentificated(rest)
+            ? <Component {...props} />
+            : <Redirect to={{
+            pathname: '/login',
+            state: { from: props.location }
+        }} />
+    )} />
+);
+
+function isAuthentificated(props){
+    let cookieValue = getCookieValue('token');
+    if(cookieValue !== null && cookieValue.length > 5){
+        let token = parseJwt(cookieValue);
+
+        for(let a = 0; a < props.permissions.length; a++) {
+            if(!token.role.includes(props.permissions[a])) {
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
+}
+
+function parseJwt (token) {
+    let base64Url = token.split('.')[1];
+    let base64 = base64Url.replace('-', '+').replace('_', '/');
+    return JSON.parse(window.atob(base64));
+}
+
+function getCookieValue(a) {
+    let b = document.cookie.match('(^|;)\\s*' + a + '\\s*=\\s*([^;]+)');
+    return b ? b.pop() : '';
+}
+
 const Content = () => (
   <main>
     <Switch>
-      <Route exact path='/' component={Dashboard} />
-      <Route path='/indicator' component={(props) => (<Indicator {...props} />)} />
-      <Route path='/indicator-group' component={IndicatorGroup} />
-      <Route path='/data-source' component={DataSource} />
-      <Route path='/admin' component={Admin} />
+      <PrivateRoute permissions={['TBD for postgraphile']} exact path='/' component={Dashboard} />
+      <PrivateRoute permissions={['TBD for postgraphile']} path='/indicator' component={(props) => (<Indicator {...props} />)} />
+      <PrivateRoute permissions={['TBD for postgraphile']} path='/indicator-group' component={IndicatorGroup} />
+      <PrivateRoute permissions={['TBD for postgraphile']} path='/data-source' component={DataSource} />
+      <PrivateRoute permissions={['TBD for postgraphile']} path='/admin' component={Admin} />
       <Route path='/login' component={Login} />
       <Route component={NotFoundComponent} />
     </Switch>
--- app/src/Components/Login/Login.js
+++ app/src/Components/Login/Login.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import './../../styles/baseStyles';
+import '../../index.css'
 import Button from '@material-ui/core/Button';
 
 const Login = () => {
     return (
         <React.Fragment>
-            <div className="container">
-                <div className="loginForm">
-                    <div className="loginFormHeader">
+            <div className='container'>
+                <div className='loginForm'>
+                    <div className='loginFormHeader'>
                         <table>
                             <tbody>
                                 <tr>
                                     <td>
-                                        <div className="btnCloseFake" />
+                                        <div className='btnCloseFake' />
                                     </td>
                                     <td>
-                                        <div className="btnMinimizeFake" />
+                                        <div className='btnMinimizeFake' />
                                     </td>
                                     <td>
-                                        <div className="btnMaximizeFake" />
+                                        <div className='btnMaximizeFake' />
                                     </td>
                                 </tr>
                             </tbody>
@@ -26,7 +27,7 @@ const Login = () => {
                     </div>
                     <div style={{ textAlign: 'center' }}>
                         <a style={{ textDecoration: 'none' }} href={window.apiUrl + 'security/oauth/google'}>
-                            <Button style={{ marginTop: '25%' }} variant="contained" color="secondary">
+                            <Button style={{ marginTop: '25%' }} variant='contained' color='secondary'>
                                 Sign in with Google
                             </Button>
                         </a>
