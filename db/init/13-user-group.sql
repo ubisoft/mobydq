356620d2f4abd6146990af7501058bf3848a89d3
alexisrolland <alexisrolland@hotmail.com>
2018-10-22 23:23:43 +0800

    Correct trigger to cascade delete group membership when group is deleted

--- db/init/13-user-group.sql
+++ db/init/13-user-group.sql
@@ -18,6 +18,10 @@ CREATE TRIGGER user_group_updated_date BEFORE UPDATE
 ON base.user_group FOR EACH ROW EXECUTE PROCEDURE
 base.update_updated_date_column();
 
+CREATE TRIGGER user_group_delete_user_group_user BEFORE DELETE
+ON base.user_group FOR EACH ROW EXECUTE PROCEDURE
+base.delete_children('user_group_user', 'user_group_id');
+
 
 
 /*Create function to create user group role*/
