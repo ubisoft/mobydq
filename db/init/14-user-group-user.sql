5f46ffba69de6dfb2abf26352e32c3da0d859350
alexisrolland <alexisrolland@hotmail.com>
2018-10-23 08:30:23 +0800

    Remove updated_date and associated trigger from table user_group_user

--- db/init/14-user-group-user.sql
+++ db/init/14-user-group-user.sql
@@ -9,16 +9,11 @@ CREATE TABLE base.user_group_user (
   , user_group_id INTEGER NOT NULL REFERENCES base.user_group(id)
   , user_id INTEGER NOT NULL REFERENCES base.user(id)
   , created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-  , updated_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
   , CONSTRAINT user_group_user_uniqueness UNIQUE (user_group_id, user_id)
 );
 
 COMMENT ON TABLE base.user_group IS
 'User groups users shows which user groups users are members of.';
-
-CREATE TRIGGER user_group_user_updated_date BEFORE UPDATE
-ON base.user_group_user FOR EACH ROW EXECUTE PROCEDURE
-base.update_updated_date_column();
 
 
 
