5b16367076a342b6db81d47eeef0a72b5d24db4a
Thomas Gassmann <thomas.gassmann@hotmail.com>
2018-10-26 21:57:51 +0200

    #161 move client to separate file

--- app/src/Components/Base/Root.js
+++ app/src/Components/Base/Root.js
@@ -2,26 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 import BaseDataView from './BaseDataView';
+import Client from './Client';
 
 
-// TODO Re-use with PrivateRoute.js
-function getCookieValue(key) {
-  const valueMatch = document.cookie.match(`(^|;)\\s*${key}\\s*=\\s*([^;]+)`);
-  return valueMatch ? valueMatch.pop() : '';
-}
-
-const client = new ApolloClient({
-  'uri': `${process.env.REACT_APP_FLASK_API_HOST}/mobydq/api/v1/graphql`,
-  'headers': {
-    'Authorization': `Bearer ${getCookieValue('token')}`
-  }
-  // Options: { mode: 'no-cors' }
-});
-
-const Root = ({ store }) => <ApolloProvider client={client}>
+const Root = ({ store }) => <ApolloProvider client={Client}>
   <Provider store={store}>
     <BrowserRouter>
       <BaseDataView />
