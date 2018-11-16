456a689a32ee3293c8e513e4a1a2462c2cae761e
simasjoneliunas <baka367@gmail.com>
2018-11-16 10:13:41 +0800

    Fixed <Loading> view flicker when sorting the data.

--- app/src/Components/ListTable/ListContainer.js
+++ app/src/Components/ListTable/ListContainer.js
@@ -8,6 +8,23 @@ import LinkButton from './../../Components/FormInput/LinkButton';
 import AddIcon from '@material-ui/icons/Add';
 
 class ListContainer extends React.Component {
+  renderForm(data) {
+    this.props.setRowTotal(data[this.props.dataObjectName].totalCount);
+    return <div>
+      <div style={{ 'float': 'left', 'marginLeft': '60px' }}>
+        {this.props.tableHeader}
+      </div>
+      <div style={{ 'float': 'right' }}>
+        <LinkButton label=<AddIcon /> type="create" color="secondary" variant="fab" to={this.props.newLink}/>
+      </div>
+      <ListTable
+        data={data[this.props.dataObjectName].nodes}
+        buttons={this.props.buttons}
+        {...this.props}
+      />
+    </div>;
+  }
+
   render() {
     return (
       <Query
@@ -16,28 +33,17 @@ class ListContainer extends React.Component {
         fetchPolicy={this.props.refetch ? 'cache-and-network' : 'cache-first'}
       >
         {({ loading, error, data }) => {
-          if (loading && data !== null) {
+          if (loading) {
+            //check if we already have data object so we would not remove the old data before we get the new one.
+            if (Object.keys(data).length) {
+              return this.renderForm(data);
+            }
             return <p>Loading...</p>;
           }
           if (error) {
             return <GraphQLError error={error} />;
           }
-          this.props.setRowTotal(data[this.props.dataObjectName].totalCount);
-          return (
-            <div>
-              <div style={{ 'float': 'left', 'marginLeft': '60px' }}>
-                {this.props.tableHeader}
-              </div>
-              <div style={{ 'float': 'right' }}>
-                <LinkButton label=<AddIcon /> type="create" color="secondary" variant="fab" to={this.props.newLink}/>
-              </div>
-              <ListTable
-                data={data[this.props.dataObjectName].nodes}
-                buttons={this.props.buttons}
-                {...this.props}
-              />
-            </div>
-          );
+          return this.renderForm(data);
         }}
       </Query>
     );
