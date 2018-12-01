88ab4f066a83f9e2f1eba8dbebe8d9720d7ae01f
alexisrolland <alexisrolland@hotmail.com>
2018-12-01 17:28:37 +0800

    Formatting docstrings.

--- api/init/security/oauth/google.py
+++ api/init/security/oauth/google.py
@@ -34,6 +34,7 @@ flow.redirect_uri = google_redirect_url
 
 def get_user_info(oauth_token: str):
     """Gets the user profile of the user after login using the corresponding OAuth token"""
+
     request_url = USER_PROFILE.format(oauth_token)
     response = requests.get(request_url)
     return json.loads(response.text)
@@ -45,23 +46,24 @@ def register_google_oauth(namespace: Namespace):
     @namespace.route('/security/oauth/google')
     @namespace.doc()
     class GoogleOAuth(Resource):
-        """Defines the resource to redirect the user to the Google OAuth page"""
+        """Defines resource to redirect user to Google OAuth page."""
 
         def get(self):
-            """Redirects the user to the Google OAuth page"""
+            """Redirects user to Google OAuth page."""
+
             url, _ = flow.authorization_url()
             return redirect(url)
 
     @namespace.route('/security/oauth/google/callback')
     @namespace.doc()
     class GoogleOAuthCallback(Resource):
-        """Defines the resource to handle the callback from Google OAuth"""
+        """Defines resource to handle callback from Google OAuth."""
 
         def get(self):
-            """Handles the Google OAuth callback and returns the token in the cookie"""
+            """Handles Google OAuth callback and fetch user access token."""
+
             code = request.args.get('code')
             token = flow.fetch_token(code=code)
             user_info = get_user_info(token['access_token'])
-            jwt = get_jwt_token(
-                TokenType.Google, user_info['email'], user_info, token)
+            jwt = get_jwt_token(TokenType.Google, user_info['email'], user_info, token)
             return get_token_redirect_response(jwt)
