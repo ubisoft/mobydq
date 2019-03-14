72a75feccdc3b422cdba66b3e2a056998a874671
simasjoneliunas <baka367@gmail.com>
2019-03-14 15:46:39 +0800

    change router type to work better in nginx proxied environment as BrowserRouter requires significant backend support that we can postpone until later versions

--- app/src/Components/Base/Root.js
+++ app/src/Components/Base/Root.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { ApolloProvider } from 'react-apollo';
 import BaseDataView from './BaseDataView';
 import Client from './Client';
@@ -9,9 +9,9 @@ import Client from './Client';
 
 const Root = ({ store }) => <ApolloProvider client={Client} store={store}>
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <BaseDataView />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 </ApolloProvider>;
 
