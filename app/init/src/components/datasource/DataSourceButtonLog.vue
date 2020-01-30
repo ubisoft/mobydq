fdbaa77a6b27d383a6dc4b27a80067822ace8088
Alexis Rolland <alexis.rolland@ubisoft.com>
2020-01-30 18:08:32 +0800

    Add button to visualize data source logs

---
+++ app/init/src/components/datasource/DataSourceButtonLog.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- Use router-link rather than v-on:click to allow user to open page in new window -->
+  <router-link v-bind:to="'/logs/datasources/' + dataSourceId">
+    <button type="button" class="btn btn-secondary ml-1">
+      Logs
+    </button>
+  </router-link>
+</template>
+
+<script>
+export default {
+  props: {
+    dataSourceId: Number
+  }
+};
+</script>
